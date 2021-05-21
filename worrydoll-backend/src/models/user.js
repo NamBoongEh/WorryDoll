f7aa3e5f1baa4029796b120e1922e870a1d3bfc3
NamBoongEh <skathdus0916@gmail.com>
2021-05-21 15:29:02 +0900

    #7 백엔드 서버 구현중
    - 토큰 발급 및 재발급 구현 완료(유효기간 7일)
    
    - 로그인, 로그아웃 구현 완료

--- worrydoll-backend/src/models/user.js
+++ worrydoll-backend/src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
   username: String,
@@ -16,14 +17,29 @@ UserSchema.methods.checkPassword = async function (password) {
   return result;
 };
 
-UserSchema.statics.findByUsername = function (username) {
-  return this.findOne({ username });
-};
-
 UserSchema.method.serialize = function () {
   const data = this.toJSON();
   delete data.hashedPassword;
   return data;
+};
+
+UserSchema.method.generateToken = function () {
+  const token = jwt.sign(
+    //첫 번째 파라미터에는 토큰 안에 집어넣고 싶은 데이터를 넣는다.
+    {
+      _id: this.id,
+      username: this.username,
+    },
+    process.env.JWT_SECRET, // 두 번째 파라미터에는 JWT 암호를 넣는다.
+    {
+      expiresIn: '7d', //7일간 유효함
+    },
+  );
+  return token;
+};
+
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({ username });
 };
 
 const User = mongoose.model('User', UserSchema);
